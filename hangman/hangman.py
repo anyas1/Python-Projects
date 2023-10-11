f2ce6350d2720487e7eedbb03aeac083ef24bf51
Anya Sanders <anyas1@bsd.uchicago.edu>
2023-10-11 09:54:18 -0500

    hangman finished

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,29 +2,48 @@ import random
 from words import words
 import string
 
-def select_valid_word(words):
+def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
     
-    return word.Upper()
+    return word.upper()
 
 def hangman():
-    word = select_valid_word(words)
+    word = get_valid_word(words)
     word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() #what the user has quessed
+    used_letters = set() #what the user has guessed
 
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    lives = 8
 
-    elif user_letter in used_letters:
-        print('You have guessed that letter already. Please try again.')
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
+        #what current word is
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1
+                print('Letter is not in word.')
+
+        elif user_letter in used_letters:
+            print('You have guessed that letter already. Please try again.')
+
+        else:
+            print('Invalid character. Try again.')
+
+    # gets here when len(word_letters) == 0 OR wen lives == 0
+    if lives == 0:
+        print('You died, sorry. The word was', word)
     else:
-        print('Invalid character. Try again.')
+        print('You guessed the word', word, '!!')
 
-    return hangman
+
+hangman()
